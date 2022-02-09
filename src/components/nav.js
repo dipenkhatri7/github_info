4e924232637750f544a2cc9ed8fb918ce155c1b9
dipenkhatri7 <khatridipen7@gmail.com>
2022-02-09 22:48:42 +0545

    Nav media

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,13 +2,16 @@ import React from 'react'
 import styled from 'styled-components';
 
 const Container = styled.div`
-    background-color: white;
     display: flex;
     align-items: center;
     justify-content: center;
     background-color: lightblue;
     height: 65.45px;
     border: 1px solid black;
+   @media (max-width: 768px) {
+      width: 100%;
+    }
+
 `
 const Title = styled.h1`
     font-family: 'Urbanist', sans-serif;
